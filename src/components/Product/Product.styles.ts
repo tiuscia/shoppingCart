9e0e4d3db15b144eb44e089ee342cb1ac0e5621c
tiuscia <tiuscia@gmail.com>
2022-12-05 00:01:21 +0100

    fixed style

--- src/components/Product/Product.styles.ts
+++ src/components/Product/Product.styles.ts
@@ -1,3 +1,27 @@
 import styled from "styled-components"
 
-export const ProductWrapper = styled.div``
+export const ProductWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100%;
+  border: 1px solid lightblue;
+  border-radius: 10px;
+  height: 100%;
+
+  button {
+    border-radius: 0 0 20px 20px;
+  }
+
+  img {
+    max-height: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+  }
+
+  div {
+    font-family: Arial, Helvetica;
+    padding: 1rem;
+    height: 100%;
+  }
+`
